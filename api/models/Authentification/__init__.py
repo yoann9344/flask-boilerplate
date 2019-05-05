4d2adfdab59ebcafc6333c146157e541873db6d2
Yoann Guillard <yoann9344@gmail.com>
2019-05-06 00:05:27 +0200

    Create __init__.py

---
+++ api/models/Authentification/__init__.py
@@ -0,0 +1,10 @@
+# import there all classes, no more to do
+from .User import User
+
+# __all__ is automatically filled :
+__all__ = []
+# list() is used cause globals change during the loop (__all__ change)
+for name, val in list(globals().items()):
+    # if the the first character is not _ then it is not a special variable/method
+    if name[0] != '_':
+        __all__.append(name)
