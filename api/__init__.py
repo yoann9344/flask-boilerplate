c4c574480f3f62250ce8d460d7eb1b917d1f9458
Yoann Guillard <yoann9344@gmail.com>
2019-05-05 16:00:58 +0200

    move blueprints import at top
    all imports should be at top
    for the db object some refactor is needed to provide circular imports

--- api/__init__.py
+++ api/__init__.py
@@ -9,6 +9,10 @@ from sqlalchemy_utils import create_database, database_exists
 from api.config import config
 from api.core import all_exception_handler
 
+# import all blueprints there
+# why blueprints http://flask.pocoo.org/docs/1.0/blueprints/
+from api.views import blueprints_list
+
 
 class RequestFormatter(logging.Formatter):
     def format(self, record):
@@ -72,16 +76,16 @@ def create_app(test_config=None):
             create_database(db_url)
 
     # register sqlalchemy to this app
+    ## imports should be at top of the file
+    ## some refactor is needed
     from api.models import db
 
     db.init_app(app)  # initialize Flask SQLALchemy with this flask app
     Migrate(app, db)
-
-    # import and register blueprints
-    from api.views import main
-
-    # why blueprints http://flask.pocoo.org/docs/1.0/blueprints/
-    app.register_blueprint(main.main)
+    
+    # register blueprints
+    for blueprint_item in blueprints_list:
+        app.register_blueprint(blueprint_item)
 
     # register error Handler
     app.register_error_handler(Exception, all_exception_handler)
