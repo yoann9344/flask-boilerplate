8cc44f7dbeec4ec745af5c26ec90d7c84003cbc6
Yoann Guillard <yoann9344@gmail.com>
2019-05-06 19:13:18 +0200

    Model SQL, contains to_dict methods

---
+++ api/models/Model.py
@@ -0,0 +1,59 @@
+# for more detail, see class Model in :
+# https://github.com/pallets/flask-sqlalchemy/blob/master/flask_sqlalchemy/model.py
+from typing import Any, List, Type
+
+
+class ModelSQL(object):
+    query_class = None  # flask_alchemy attribute
+    query = None  # flask_alchemy attribute
+
+    DONOTSEND_MODEL = {'_sa_instance_state'}
+    DONOTSEND = []
+    _jsonified = None
+
+    def __repr__(self) -> str:
+        return '<{}>'.format(self.__class__.__name__)
+
+    def to_dict_recursive(self) -> dict:
+        return self._to_dict_recursive(list_objects_id_passed_through=[id(self)])
+
+    def _to_dict_recursive(self, list_objects_id_passed_through: List[int]) -> dict:
+        # functions :
+        # anti_circular_recursion : check if we've already called the object
+        #                               if not do the recursion
+        # type_shunt_recursive : select the actions for each type of attr
+
+        def anti_circular_recursion(obj: Type[ModelSQL]) -> any:
+            if id(obj) in list_objects_id_passed_through:
+                return str(obj)
+                # others possibilities
+                # return str(obj).join(' ').join(str(obj.id))
+                # return obj.id
+            else:
+                list_objects_id_passed_through.append(id(obj))
+                return obj._to_dict_recursive(list_objects_id_passed_through)
+
+        ##### what about dict which contains object(s) ? Is it possible in SQLAlchemy ?
+        def type_shunt_recursive(attribute: Any) -> Any:
+            # model
+            if issubclass(type(attribute), ModelSQL):
+                return anti_circular_recursion(attribute)
+            # recursive iteration of the list in case of the list is a relationship
+            elif isinstance(attribute, list):
+                values = []
+                for item in attribute:
+                    values.append(type_shunt_recursive(item))
+                return values
+            # attribute is not an instance of relationship (int, str..)
+            else:
+                return attribute
+
+        result = {}
+        # __mapper__ is equivalent to db.inspect(self)
+        # but db (database) is not created yet cause we send this model to the constructor
+        for key in self.__mapper__.attrs.keys():
+            if key not in self.DONOTSEND:
+                attr = getattr(self, key)
+                result[key] = type_shunt_recursive(attr)
+
+        return result
